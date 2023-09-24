d337ad3db09a18cc0415827984da40322590a384
harned <aleshkindima25@gmail.com>
2023-09-24 20:44:08 +0600

    Отказался от PostgreSQL в пользу MySQL (была конкретная запара с сохранением картинок). Добавлена возможность сохранения фото вместе с товаром. Расширен функционал. По минимому набросан фронт, в скоре будет переписываться, если останутся силы. В противном случае, завтра. Добавлен Spting Security. Настроена авторизация. Будет еще допиливаться до нормального состояния.

--- src/main/java/com/example/market/models/Product.java
+++ src/main/java/com/example/market/models/Product.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "products")
@@ -16,19 +19,28 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
-
     @Column(name = "title")
     private String title;
-
     @Column(name = "description", columnDefinition = "text")
     private String description;
-
     @Column(name = "price")
     private int price;
-
     @Column(name = "location")
     private String location;
-
     @Column(name = "person")
     private String person;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product", orphanRemoval = true)
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
+
+    public void addImageToProduct(Image image) {
+        image.setProduct(this);
+        images.add(image);
+    }
 }
